6be7a949061a648b907fb081a4874cd82eafd57d
a653398363 <30554223+a653398363@users.noreply.github.com>
2021-07-07 16:46:02 +0800

    Update js 链式.js

--- js/designMode/js 链式.js
+++ js/designMode/js 链式.js
@@ -99,3 +99,45 @@ const obj2 = {
 // isEqual函数，相等输出true，不相等输出false
 isEqual(obj1, obj2)
 
+
+	// 更详细的解答建议参考Underscore源码[https://github.com/lessfish/underscore-analysis/blob/master/underscore-1.8.3.js/src/underscore-1.8.3.js#L1094-L1190](https://github.com/lessfish/underscore-analysis/blob/master/underscore-1.8.3.js/src/underscore-1.8.3.js#L1094-L1190)
+	function isEqual(A, B) {
+	    const keysA = Object.keys(A)
+	    const keysB = Object.keys(B)
+	
+	    // 健长不一致的话就更谈不上相等了
+	    if (keysA.length !== keysB.length) return false
+	
+	    for (let i = 0; i < keysA.length; i++) {
+	        const key = keysA[i]
+	
+	        // 类型不等的话直接就不相等了
+	        if (typeof A[key] !== typeof B[key]) return false
+	    
+	        // 当都不是对象的时候直接判断值是否相等
+	        if (typeof A[key] !== 'object' && typeof B[key] !== 'object' && A[key] !== B[key]) {
+	            return false
+	        }
+	
+	        if (Array.isArray(A[key]) && Array.isArray(B[key])) {
+	            if (!arrayEqual(A[key], B[key])) return false
+	        }
+	
+	        // 递归判断
+	        if (typeof A[key] === 'object' && typeof B[key] === 'object') {
+	            if (!isEqual(A[key], B[key])) return false
+	        }
+	    }
+	
+	    return true
+	}
+	
+	function arrayEqual(arr1, arr2) {
+	    if (arr1.length !== arr2.length) return false
+	
+	    for (let i = 0; i < arr1.length; i++) {
+	        if (arr1[i] !== arr2[i]) return false
+	    }
+	
+	    return true
+	}
