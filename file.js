d6c6b6a767e95618356e010a6edca82eded77f25
FanGe <653398363@qq.com>
2022-02-22 19:43:20 +0800

    File

--- file.js
+++ file.js
@@ -1,61 +1,64 @@
- fs = require("fs");
+fs = require("fs");
+const path = require("path");
 
- const path = require("path");
- const url = path.join(__dirname, '/')
+const movieAr = ['AVI', 'mov', 'rmvb', 'rm', 'FLV', 'mp4', '3GP']
 
- recircle(url)
+recircle(path.dirname(__filename))
+
+// 电影时间
+const limitDate = []
+for (let i = new Date().getFullYear() - 50; i < new Date().getFullYear() + 20; i++) {
+  limitDate.push(i)
+}
+
+const str = '李茂扮太子.Another.Me.2022.2160p.WEB'
+
+console.log('', new Date().getFullYear(), limitDate, str.split('.'));
 
 
+function recircle(url) {
+  const fileList = fs.readdirSync(url)
+  fileList.forEach((file) => {
+    const filePath = url + '\\' + file
+    // console.log('url ' + url + ' fileList ' + file + " filePath " + filePath);
 
- async function recircle(url) {
-   const fileT = await fileType(url);
+    fs.stat(filePath, (err, stats) => {
+      // 是文件 文件直接处理
+      if (stats.isFile()) {
+        resetName(filePath)
+      } else if (stats.isDirectory()) {
 
-   //  file     文件直接处理
-   if (fileT === 'file') return resetName(url)
+        resetName(filePath, 'isDir')
+        // 为文件夹
+        recircle(filePath)
+      } else {
+        console.log('其他不明类型文件');
+      }
 
-   // 文件夹处理
-   fs.readdir(url, 'utf8', (err, fileList) => {
-     if (err) throw err;
-     fileList.forEach((file, ind) => {
-       // const fileTT = fileType(file);
-       // console.log('fileT', file, fileTT);
-       fs.stat(file, function (e, stats) {
-         console.log('stats stats ', file, stats);
-         if (!stats) return
-         const isDir = stats.isDirectory();
-         if (stats.isFile()) return resetName(file)
-         if (isDir) {
-           //  if(file === "webpack资料") {
-           //    console.log('isDir  isDir', stats);
-           //  }
-           console.log('recircle', file);
+    })
 
-           recircle(file)
-         }
-       })
-       // if (file === "test.txt") {
-       //   fs.rename(file, "note.txt", (er) => {
-       //     console.log("er", er)
-       //   })
-       // }
-     });
-   })
- }
+  })
+}
 
- function fileType(file) {
-   return new Promise((resolve, reject) => {
-     fs.stat(file, function (err, ststats) {
-       if (err) return
-       if (ststats.isFile()) resolve('file')
-       if (ststats.isDirectory()) resolve('isDir')
-     })
-   })
- }
+function resetName(filePath, filetype) {
+  const dirname = path.dirname(filePath) // 路径中代表文件夹的部分
+  const fileName = path.basename(filePath) // 文件名
 
- function resetName(file) {
-   console.log('修改 ' + file + "名字");
+  // [\u4e00-\u9fa5] 中文
+  let newFile = ''
+  const fileNameArr = fileName.split('.')
+  // console.log('filetype isDir ', filePath, fileName, fileNameArr, fileNameArr.length);
+  if (filetype === 'isDir') {
+    if (fileNameArr.length > 2) {
+      console.log('fileNameArr', fileNameArr);
+    }
+  } else {
 
-   //  fs.rename(file, "note.txt", (er) => {
-   //    console.log("er", er)
-   //  })
- }
+  }
+  // path.extname(filePath)   文件名后缀
+  // console.log('修改 ' + fileName + dirname + " 后缀名：" + path.extname(filePath));
+
+  //  fs.rename(file, "note.txt", (er) => {
+  //    console.log("er", er)
+  //  })
+}
