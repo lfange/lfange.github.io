9aa32ad9ac31b6fe1a37f5c056f2968dec9c2edb
a653398363 <653398363@qq.com>
2022-05-25 16:12:17 +0800

    update

--- dataStructure/binarySearch.js
+++ dataStructure/binarySearch.js
@@ -1,21 +1,21 @@
-var binarySearch = function(list, target) {
-	let low = 0
-	let high = list.length - 1
-	let mid = null
+var binarySearch = function (list, target) {
+  let low = 0
+  let high = list.length - 1
+  let mid = null
 
-	while (low <= high) {
-		mid = parseInt((low + high) / 2)
-		let cur = list[mid]
-		console.log('low high', low, high)
-		if (cur === target) {
-			return mid
-		} else if (cur > target) {
-			high = mid - 1
-		} else {
-			low = mid + 1
-		}
-	}
-	return null
+  while (low <= high) {
+    mid = parseInt((low + high) / 2)
+    let cur = list[mid]
+    console.log('low high', low, high)
+    if (cur === target) {
+      return mid
+    } else if (cur > target) {
+      high = mid - 1
+    } else {
+      low = mid + 1
+    }
+  }
+  return null
 }
 
 
