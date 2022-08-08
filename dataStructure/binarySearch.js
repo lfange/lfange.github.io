2477a68da0257b0fb33f83069ee75bf74544a89e
FanGe <653398363@qq.com>
2022-08-08 14:24:28 +0800

    1

--- dataStructure/binarySearch.js
+++ dataStructure/binarySearch.js
@@ -21,3 +21,17 @@ var binarySearch = function(list, target) {
 
 const list = [1, 3, 5, 7, 9]
 console.log('mid', binarySearch(list, 7))
+
+
+function minSelect(arr) {
+	let min = arr[0]
+	for (let i = 0; i < arr.length; i++) {
+		min = min < arr[i] ? min : arr[i]
+	}
+	console.log('min', min)
+	return min
+}
+
+const a = [5,16,25,18,32,03]
+
+console.log('backwards', minSelect(a))
