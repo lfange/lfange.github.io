5f9711f27ac67f9ab95f7a107af8a9ecf9d5a77c
FanGe <653398363@qq.com>
2022-09-16 20:55:22 +0800

    modify

--- docs/.vuepress/configs/themeConfig/sidebar.ts
+++ docs/.vuepress/configs/themeConfig/sidebar.ts
@@ -1,132 +1,86 @@
-
-
 // vue path render
 const Vue2Base = (name: string): string => {
-  return `/Front/Vue/${name}.md`
-}
+  return `/Front/Vue/${name}.md`;
+};
 
 const Vue3Base = (name: string): string => {
-  return `/Front/vue3/${name}.md`
-}
+  return `/Front/vue3/${name}.md`;
+};
 
 const jsBase = (name: string): string => {
-  return `/Front/JavaScript/${name}.md`
-}
+  return `/Front/JavaScript/${name}.md`;
+};
 
 const TsBase = (name: string): string => {
-  return `/Front/ts/${name}.md`
-}
+  return `/Front/ts/${name}.md`;
+};
 
 const comBase = (name: string): string => {
-  return `/computer/${name}.md`
-}
+  return `/computer/${name}.md`;
+};
 
 const sidebar = {
-  '/algorithms/': [
+  "/algorithms/": [
     {
-      "text": "ALGORITHMS",
-      "title": "ALGORITHMS",
-      "collapsable": false,
-      // "sidebarDepth": 1,
-      "children": [
-        "README.md",
-        "comalgrithms"
-      ]
-    }
-  ],
-  '/Front/Vue/': [
-    {
-      text: "Vue",
-      link: Vue2Base('basic/vuebase'),
-      children: [
-        Vue2Base('basic/vuebase'),
-        Vue2Base('basic/animation'),
-        Vue2Base('basic/Vuex'),
-        Vue2Base('componet/usage'),
-        Vue2Base('basic/communication'),
-        Vue2Base('basic/componentRender'),
-        Vue2Base('basic/vslot'),
-        Vue2Base('basic/VueMock'),
-      ]
-    }
-  ],
-  '/Front/Vue/advanced/': [
-    {
-      text: "Vue advanced",
-      link: Vue2Base('advanced/vuecli'),
-      children: [
-        Vue2Base('advanced/vuecli'),
-        Vue2Base('advanced/Vuewebpack'),
-        Vue2Base('advanced/Vuescoped'),
-      ]
-    }
-  ],
-  '/Front/vue3/': [
-    {
-      text: "Vue3",
-      link: Vue3Base('basic'),
+      text: "ALGORITHMS",
+      title: "ALGORITHMS",
       collapsable: false,
-      children: [ 
-        Vue3Base('basic'),
-        Vue3Base('change'),
-      ]
-    }
+      // "sidebarDepth": 1,
+      children: ["README.md", "comalgrithms"],
+    },
   ],
-  '/Front/JavaScript/': [
+  "/Front/Vue/": [
+    Vue2Base("basic/vuebase"),
+    Vue2Base("basic/animation"),
+    Vue2Base("basic/Vuex"),
+    Vue2Base("componet/usage"),
+    Vue2Base("basic/communication"),
+    Vue2Base("basic/componentRender"),
+    Vue2Base("basic/vslot"),
+    Vue2Base("basic/VueMock"),
+  ],
+  "/Front/Vue/advanced/": [
+    Vue2Base("advanced/vuecli"),
+    Vue2Base("advanced/Vuewebpack"),
+    Vue2Base("advanced/Vuescoped"),
+  ],
+  "/Front/vue3/": [Vue3Base("basic"), Vue3Base("change")],
+  "/Front/JavaScript/": [
+    jsBase("currie"),
+    jsBase("防抖节流"),
+    jsBase("callApply"),
+    jsBase("brower"),
+    jsBase("flatten"),
+  ],
+  "/Front/ts/": [TsBase("README"), TsBase("baseType")],
+  "/Front/browser/": ["/Front/browser/README.md", "/Front/browser/cache.md"],
+  "/computer/": [
     {
-      text: "JavaScript",
-      link: '/Front/JavaScript/currie',
-      children: [ 
-        jsBase('currie'),
-        jsBase('防抖节流'),
-        jsBase('callApply'),
-        jsBase('brower'),
-        jsBase('flatten'),
-      ]
-    }
+      text: "计算机组成原理",
+      children: [comBase("pocc/README")],
+    },
+    {
+      text: "操作系统",
+      children: [comBase("os/README")],
+    },
+    {
+      text: "计算机网络",
+      children: [comBase("net/README"), comBase("net/network-architecture")],
+    },
+    // "/computer/README.md", "/computer/network-architecture.md"],
   ],
-  '/Front/ts/': [
-    TsBase('README'),
-    TsBase('baseType'),
-  ],
-  '/Front/browser/': [
-    '/Front/browser/README.md',
-    '/Front/browser/cache.md'
-  ],
-  '/computer/': [
-      { 
-        text: '计算机组成原理',
-        children: [
-          comBase('pocc/README')
-        ]
-      },
-      { 
-        text: '操作系统',
-        children: [
-          comBase('os/README')
-        ]
-      },
-      { 
-        text: '计算机网络',
-        children: [
-          comBase('net/README'),
-          comBase('net/network-architecture'),
-        ]
-      },
-        // "/computer/README.md", "/computer/network-architecture.md"],
-  ],
-  '/tools/': [
-    '/tools/',
-    '/tools/vuepress/',
-    '/tools/camunda/',
-    // { 
+  "/tools/": [
+    "/tools/",
+    "/tools/vuepress/",
+    "/tools/camunda/",
+    // {
     //   text: '工具类',
     //   link: '/tools/',
     //   children: [
     //     '/tools/vuepress/',
     //   ],
     // }
-  ]
+  ],
 };
 
 export default sidebar;
