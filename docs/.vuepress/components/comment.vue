edf56eb164f5ebcfde9016d30679fb2655e34725
FanGe <653398363@qq.com>
2022-10-20 16:20:04 +0800

    chore: gitalk

---
+++ docs/.vuepress/components/comment.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="gitalk-container">
+    <div id="gitalk-container"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'comment',
+  data() {
+    return {};
+  },
+  mounted() {
+    let body = document.querySelector('.gitalk-container');
+    let script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js';
+    body.appendChild(script);
+    script.onload = () => {
+      const commentConfig = {
+        clientID: 'f1309179924acb97c720',
+        clientSecret: '9642dae529f47a9649217ffec15dca010dc00786',
+        repo: 'lfange.github.io',
+        owner: 'lfange',
+        // 这里接受一个数组，可以添加多个管理员，可以是你自己
+        admin: ['lfange'],
+        // id 用于当前页面的唯一标识，一般来讲 pathname 足够了，
+        // 但是如果你的 pathname 超过 50 个字符，GitHub 将不会成功创建 issue，此情况可以考虑给每个页面生成 hash 值的方法.
+        id: location.pathname,
+        distractionFreeMode: false,
+      };
+      const gitalk = new Gitalk(commentConfig);
+      gitalk.render('gitalk-container');
+    };
+  },
+};
+</script>
+<style>
+@import '../styles/gittalk.css';
+</style>
