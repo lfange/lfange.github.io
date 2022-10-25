8c023ffac99cd9a55cb7be93f6a044540a21a26c
a653398363 <653398363@qq.com>
2022-10-25 16:28:39 +0800

    t

--- docs/.vuepress/configs/head.ts
+++ docs/.vuepress/configs/head.ts
@@ -2,8 +2,8 @@ import type { HeadConfig } from '@vuepress/core'
 
 const head: HeadConfig[] = [
 
-  ['meta', { name: 'application-name', content: 'Lfange Blog!' }],
-  ['meta', { name: 'apple-mobile-web-app-title', content: 'VuePress Lfange Blog!!' }],
+  ['meta', { name: 'application-name', content: '哓番茄 Lfange Blog!' }],
+  ['meta', { name: 'apple-mobile-web-app-title', content: '哓番茄 Lfange Blog!!' }],
   ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
   ['link', { rel: 'icon', href: '/images/icons/p.png' }],
   ['meta', { name: 'baidu-site-verification', content: 'code-4j7CtE1noP' }],
