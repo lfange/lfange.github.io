6a0ae6fbb3502141dd2c4c818f237a06cca9a11e
a653398363 <653398363@qq.com>
2022-11-09 10:13:40 +0800

    2

--- depoly.sh
+++ depoly.sh
@@ -5,8 +5,8 @@ set -e
 # 打包
 npm run docs:build
 
-# 进入目标文件夹
-cd dist
+# 进入生成的文件夹
+cd docs/.vuepress/dist
 
 # 提交到本地仓库
 
@@ -14,7 +14,7 @@ git init
 git add -A
 git commit -m $1
 
-# 提交到 https://github.com/lfange/daydayup.git 的 gh-pages 分支
-git push -f https://github.com/lfange/daydayup.git master:gh-pages
+# 提交到 https://github.com/lfange/lfange.github.io.git 的 gh-pages 分支
+git push -f https://github.com/lfange/lfange.github.io.git master:gh-pages
 
 cd -
