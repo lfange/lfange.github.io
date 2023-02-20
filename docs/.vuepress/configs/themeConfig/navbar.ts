a529f51992717923582341147ec898eaa71a6969
fange <653398363@qq.com>
2023-02-20 17:32:14 +0800

    mock

--- docs/.vuepress/configs/themeConfig/navbar.ts
+++ docs/.vuepress/configs/themeConfig/navbar.ts
@@ -1,76 +1,80 @@
 // navbar of headlink
 const navbar = [
   {
-    text: "算法",
-    link: "/algorithms/",
+    text: '算法',
+    link: '/algorithms/',
   },
   '/interview/READMD.md',
   {
-    text: "计算机",
-    link: "/computer/",
+    text: '计算机',
+    link: '/computer/',
     children: [
       // '/computer/os/README.md',
-      "/computer/net/network-architecture",
+      '/computer/net/network-architecture',
       // '/computer/pocc/README.md',
     ],
   },
   {
-    text: "前端",
-    link: "/Front/",
+    text: '前端',
+    link: '/Front/',
     children: [
       {
-        text: "Vue Base",
-        link: "/Front/vue/basic/vuebase",
+        text: 'Vue Base',
+        link: '/Front/vue/basic/vuebase',
       },
       {
-        text: "Vue advanced",
-        link: "/Front/vue/advanced/vuecli",
+        text: 'Vue advanced',
+        link: '/Front/vue/advanced/vuecli',
         // children: [
         //   '/Front/vue/README.md',
         //   '/Front/vue/VueScoped.md'
         // ]
       },
       {
-        text: "Vue3",
-        link: "/Front/Vue3/basic",
+        text: 'Vue3',
+        link: '/Front/Vue3/basic',
       },
       {
-        text: "JavaScript",
-        link: "/Front/JavaScript/currie",
+        text: 'JavaScript',
+        link: '/Front/JavaScript/currie',
       },
       {
-        text: "TypeScript",
-        link: "/Front/ts/",
+        text: 'TypeScript',
+        link: '/Front/ts/',
       },
       {
-        text: "浏览器",
-        link: "/Front/browser/README.md",
+        text: '浏览器',
+        link: '/Front/browser/README.md',
       },
       '/Front/errlog.md',
       {
-        text: "性能优化",
-        link: "/Front/performance/README.md",
-      }
+        text: '性能优化',
+        link: '/Front/performance/README.md',
+      },
     ],
   },
   {
-    text: "后端",
-    link: "/backend/",
-    children: ["/backend/Golang/README.md"],
-  },
-  { text: "服务", link: "/serve/", children: [ "/serve/linux.md", "/serve/ngnix.md", "/serve/frps.md"] },
-  {
-    text: "web3",
-    link: "/web3/",
+    text: '后端',
+    link: '/backend/',
+    children: ['/backend/Golang/README.md'],
   },
   {
-    text: "工具",
-    link: "/tools/",
+    text: '服务',
+    link: '/serve/',
+    children: ['/serve/linux.md', '/serve/ngnix.md', '/serve/frps.md'],
+  },
+  {
+    text: 'web3',
+    link: '/web3/',
+  },
+  {
+    text: '工具',
+    link: '/tools/',
   },
   // {
   //   text: 'luck',
   //   link: '/tools/luckb'
   // }
-];
+]
 
-export default navbar;
+export default navbar
