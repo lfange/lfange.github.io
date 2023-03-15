eb442ae2ab65a5506409eafb5ac54059d1684e17
fange <653398363@qq.com>
2023-03-15 17:52:18 +0800

    seo

--- docs/.vuepress/configs/head.ts
+++ docs/.vuepress/configs/head.ts
@@ -1,12 +1,21 @@
 import type { HeadConfig } from '@vuepress/core'
 
 const head: HeadConfig[] = [
-
   ['meta', { name: 'application-name', content: '哓番茄 Lfange Blog!' }],
-  ['meta', { name: 'apple-mobile-web-app-title', content: '哓番茄 Lfange Blog!!' }],
+  [
+    'meta',
+    { name: 'apple-mobile-web-app-title', content: '哓番茄 Lfange Blog!!' },
+  ],
   ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
   ['link', { rel: 'icon', href: '/images/icons/p.png' }],
   ['meta', { name: 'baidu-site-verification', content: 'code-4j7CtE1noP' }],
+  [
+    'meta',
+    {
+      name: 'keywords',
+      content: '哓番茄, Lfange Blog!, JavaScript, linux, git',
+    },
+  ],
   // <meta name="baidu-site-verification" content="code-4j7CtE1noP" />
 ]
 
