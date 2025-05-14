609c92c222d076fbec42b0780ac5ffc95b928eef
FanGe <653398363@qq.com>
2025-05-14 23:00:16 +0800

    ✨ feat(design): designdesigndesign

---
+++ code/design/model.js
@@ -0,0 +1,40 @@
+//
+
+function Model() {
+  this.title = 'title 1111111'
+}
+
+function Controller(model) {
+  const that = this
+  this.model = model
+
+  this.getModelByKey = function (key) {
+    return that.model[key]
+  }
+
+  this.handleEvent = function (e) {
+    e.stopPropagation()
+    switch (e.type) {
+      case 'click':
+        clickHandler(e.target)
+        break
+      default:
+        console.log(e.target)
+    }
+  }
+  function clickHandler(target) {
+    that.model.title = 'world'
+    target.innerText = that.getModelByKey('title')
+  }
+}
+
+function Views(controller) {
+  this.controller = controller
+
+  this.demobuttton = document.getElementById('demobutton')
+  this.demobuttton.innerText = controller.getModelByKey('title')
+  this.demobuttton.addEventListener('click', controller)
+}
+
+const mode = new Model()
+const control = new Controller(mode)
