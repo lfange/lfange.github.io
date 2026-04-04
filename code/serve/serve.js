b734c5240ff69d7e67c4a104498e8abad8336ac9
FanGe <653398363@qq.com>
2026-04-04 15:20:43 +0800

    tes

--- code/serve/serve.js
+++ code/serve/serve.js
@@ -1,11 +1,11 @@
-const http = require('http');
-const hostname = '0.0.0.0';
+const http = require('http')
+const hostname = '0.0.0.0'
 
-const port = 80;
+const port = 80
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
+  res.statusCode = 200
   res.writeHead(200, {
-    'Content-Type': 'text/html; charset=utf8'
+    'Content-Type': 'text/html; charset=utf8',
   })
   res.end(`此域名可售\n请联系653398363@qq.com<br/>
     <br/><br/><br/><br/><br/>
@@ -14,8 +14,8 @@ const server = http.createServer((req, res) => {
       bottom: 10px;
       text-align: center;
       width: 100%;"><a href="https://beian.miit.gov.cn/">蜀ICP备2022007922号-1</a></div>
-    `);
-});
+    `)
+})
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
--- code/src/js/test.js
+++ code/src/js/test.js
@@ -1,8 +1,13 @@
+// export function mul(x, y) {
+//   return x * y
+// }
 
-export function mul(x, y) {
-  return x * y;
-}
+// export function count(x, y) {
+//   return x - y
+// }
 
-export function count(x, y) {
-  return x - y;
+for (var i = 0; i < 3; i++) {
+  setTimeout(function () {
+    console.log(`output->i`, i)
+  }, 200)
 }
